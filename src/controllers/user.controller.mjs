a16bb2a3858669116581e2e4637bafeb32444be9
Juan Romero <juan2905sbr@gmail.com>
2025-05-26 18:39:57 -0500

    user.route: vincula la ruta get api/user al controlador y en el user.controller:consulta la base de datos atraves de el modelo y obtiene el listado de productos

--- src/controllers/user.controller.mjs
+++ src/controllers/user.controller.mjs
@@ -14,11 +14,30 @@ const createUser = async ( req, res ) => {
     }
     catch (error) {
         console.error(error);
-        res.jsom({msg:"Error: No se pudo registrar el producto"});
+        res.json({msg:"Error: No se pudo registrar el producto"});
     }
 
 }
 
+const getAllUser = async (req, res) => {
+
+    try {
+        const data = await userModel.find({});
+        res.json(data);
+    }
+    catch (error){
+        console.error(error);
+        res.json({msg: "Error: No se pudo obtener el listado de productos"})
+
+    }
+
+
+    
+}
+
+
+//exponer las funcionalidades para ser usadas por otros archivos
 export{
-    createUser
+    createUser,
+    getAllUser
 }
--- src/routes/user.route.mjs
+++ src/routes/user.route.mjs
@@ -1,10 +1,11 @@
 import express from "express";   //importo dependencia
-import { createUser } from "../controllers/user.controller.mjs";
+import { createUser, getAllUser } from "../controllers/user.controller.mjs";
 
 const router = express.Router(); //invocando el router de express
 
 //Definir las rutas para la entidad Product.
 router.post( "/api/user" , createUser );
+router.get ("/api/user", getAllUser);
 
 //exponer el router de este archivo para ser usado por otros en la aplicacion
 export default router
